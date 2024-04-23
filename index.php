f4a4945bd79a2bc9d5cf77ec80fe1ec655d53c5f
Khushi singhal <134137454+Khushi1298@users.noreply.github.com>
2024-04-23 13:59:51 +0530

    main page

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUIZ GAME</title>
+    <link href="quiz.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="nav">
+            <nav>
+                <div class="logo">
+                    <img height="50px" width="70px"  src="logo.jpeg">
+                    <H1>FUNIQ</H1>
+                </div>
+                <ul class="nav_links">
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">About</a></li>
+                    <li class="dropdown">
+                        <a href="#">Menu</a>
+                        <div class="down">
+                        <a href="#">Topic</a>
+                        <a href="#">Events</a>
+                        <a href="#">Services</a>
+                        <a href="#">Contact Us</a>
+                    </div>
+                </li>
+                    <li class="user">
+                        <a href="#">User</a>
+                        <div class="down">
+                        <a href="#">Log-in</a>
+                        <a href="#">Your Profile </a>
+                        <a href="#">Progress</a>
+                        <a href="#">Streak</a>
+                        </div>
+                    </li>
+                    <div class="btn">
+                        <button>Register</button>
+                    </div>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="header">
+        <h1>FUNIQ=FUN+QUIZ</h1>
+        <P>Funiq is a platform that provide a User friendly interface for Students.In the below section user can <span>Practice Skills</span> </P>
+        <img height="50%" width="50%" src="img.jpeg">
+    </div>
+    <div class="rules">
+        <h2>RULES of the Quiz</h2>
+        <ol type="1" class="Rlist">
+            <li>There are some Questions</li>
+            <li>Every que have 4 options </li>
+            <li>You can skip any Question if you want otherwise go to next que </li>
+        </ol>
+        <h3>Enjoy it , have Fun & Explore new thigs </h4>
+         <button class="start">Start</button>
+              <div class="que">
+                 <ol>
+                    <li>What is the capital city of France?
+                        <ul class="options">
+                            <li><input type="radio" name="q1" value="a"> a)Rome</li>
+                            <li><input type="radio" name="q1" value="b"> b)Madrid </li>
+                            <li><input type="radio" name="q1" value="c"> c)Paris </li>
+                            <li><input type="radio" name="q1" value="d"> d)Berlin </li>
+                        </ul>
+                        <button class="skip">Skip</button>
+                        <button class="next">Next</button>
+                    </li>
+                    <li>Who is known as the inventor of the light bulb?
+                        <ul class="options">
+                            <li><input type="radio" name="q2" value="a"> a)Thomas Edison</li>
+                            <li><input type="radio" name="q2" value="b"> b)Nikola Tesla </li>
+                            <li><input type="radio" name="q2" value="c"> c)Alexander Graham Bell</li>
+                            <li><input type="radio" name="q2" value="d"> d)Albert Einstein </li>
+                        </ul>
+                        <button class="skip">Skip</button>
+                        <button class="next">Next</button>
+                    </li>
+                    <li> Who wrote the famous play "Romeo and Juliet"?
+                        <ul class="options">
+                            <li><input type="radio" name="q3" value="a"> a)William Shakespeare</li>
+                            <li><input type="radio" name="q3" value="b"> b) Jane Austen </li>
+                            <li><input type="radio" name="q3" value="c"> c)Charles Dickens </li>
+                            <li><input type="radio" name="q3" value="d"> d)Mark Twain </li>
+                        </ul>
+                        <button class="skip">Skip</button>
+                        <button class="next">Next</button>
+                    </li>
+                    <li>What is the chemical symbol for water?
+                        <ul class="options">
+                            <li><input type="radio" name="q4" value="a"> a)H2O </li>
+                            <li><input type="radio" name="q4" value="b"> b)CO2 </li>
+                            <li><input type="radio" name="q4" value="c"> c)O2 </li>
+                            <li><input type="radio" name="q4" value="d"> d)NACL </li>
+                        </ul>
+                        <button class="skip">Skip</button>
+                        <button class="next">Next</button>
+                    </li>
+                    <li>Which planet is known as the "Red Planet"?
+                        <ul class="options">
+                            <li><input type="radio" name="q5" value="a"> a)Venus </li>
+                            <li><input type="radio" name="q5" value="b"> b)Mars </li>
+                            <li><input type="radio" name="q5" value="c"> c)Jupiter </li>
+                            <li><input type="radio" name="q5" value="d"> d)Saturn </li>
+                        </ul>
+                        <button class="skip">Skip</button>
+                        <button class="next">Next</button>
+                    </li>
+                    <li>What is the longest river in the world?
+                        <ul class="options">
+                            <li><input type="radio" name="q6" value="a"> a)Nile </li>
+                            <li><input type="radio" name="q6" value="b"> b)Amazon </li>
+                            <li><input type="radio" name="q6" value="c"> c)Mississippi </li>
+                            <li><input type="radio" name="q6" value="d"> d)Yangtze </li>
+                        </ul>
+                        <button class="skip">Skip</button>
+                        <button class="next">Next</button>
+                    </li>
+                    <li>What is the currency of Japan?
+                        <ul class="options">
+                            <li><input type="radio" name="q7" value="a"> a)Dollar </li>
+                            <li><input type="radio" name="q7" value="b"> b)Euro </li>
+                            <li><input type="radio" name="q7" value="c"> c)Yen </li>
+                            <li><input type="radio" name="q7" value="d"> d)Pound </li>
+                        </ul>
+                             <button class="skip">Skip</button>
+                             <button class="next">Next</button>
+                    </li>
+                    <li>Which country is known as the "Land of the Rising Sun"?
+                        <ul class="options">
+                            <li><input type="radio" name="q8" value="a"> a)China </li>
+                            <li><input type="radio" name="q8" value="b"> b)India </li>
+                            <li><input type="radio" name="q8" value="c"> c)Japan </li>
+                            <li><input type="radio" name="q8" value="d"> d)Australia </li>
+                        </ul>
+                        <button class="skip">Skip</button>
+                        <button class="next">Next</button>
+                    </li>
+                </ol>
+              </div>
+        </div>
+</body>
+</html>
